c0cf97dc6eba028f6328d53ed1b2e29b4053a007
Kathy Tavia <kathy.tavia@gmail.com>
2022-02-05 11:00:30 +0100

    added mapbox & marker

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,13 @@ import './App.scss';
 import './Flat.scss';
 import Flat from './Flat';
 import { useEffect, useState } from 'react';
+import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
+import FlatMarker from './FlatMarker';
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiY2RybXIxOCIsImEiOiJja3o5bXRybjMxbDlwMnRwa2w0amM5azNoIn0.tpftcH7amACCXKbpKNsRzQ',
+});
 
 const API_URL =
   'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json';
@@ -29,6 +36,19 @@ const App = () => {
             return <Flat key={id} {...props} />;
           })}
         </div>
+      </div>
+
+      <div className="map">
+        <Map
+          zoom={[14]}
+          center={[2.3522, 48.8566]}
+          containerStyle={{ height: '100vh', width: '100%' }}
+          style="mapbox://styles/mapbox/streets-v8"
+        >
+          <Marker coordinates={[2.3522, 48.8566]} anchor="bottom">
+            <FlatMarker />
+          </Marker>
+        </Map>
       </div>
     </div>
   );
---
+++ src/components/FlatMarker.jsx
@@ -0,0 +1,9 @@
+import './FlatMarker.scss'
+
+const FlatMarker = ({price}) => {
+  return (
+    <div className='marker'>{price}</div>
+  )
+};
+
+export default FlatMarker;
